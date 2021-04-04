939fd09e3230f2717d3312cfa132db5bfef80dc8
Pratik Raut <pratikraut0000@gmail.com>
2021-04-04 12:28:19 +0530

    website loading and ready to start testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
+
+    private static JavascriptExecutor jsExecutor;
+
     public static void main(String[] args) {
+
+        // Setup basic Things
+        System.setProperty("webdriver.chrome.driver", "src/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        jsExecutor = (JavascriptExecutor) driver;
+
+        try {
+            driver.manage().window().maximize();
+
+            // Load Website
+            driver.get("https://www.thesparksfoundationsingapore.org/");
+
+            // Testing begins
+            //...
+
+        }catch (Exception e) {
+            System.out.println("Program Crashed !");
+            System.out.println("Reason - " + e.getMessage());
+
+        }finally {
+            driver.quit();
+        }
 
     }
 }
